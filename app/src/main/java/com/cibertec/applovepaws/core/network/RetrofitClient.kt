1c41d15673f527261bba61b7db617b5df4844e63
AnthoCib <anthoncr74@gmail.com>
2026-02-25 00:13:46 -0500

    avance xd

--- app/src/main/java/com/cibertec/applovepaws/core/network/RetrofitClient.kt
+++ app/src/main/java/com/cibertec/applovepaws/core/network/RetrofitClient.kt
@@ -1,8 +1,6 @@
 package com.cibertec.applovepaws.core.network
 
-import com.cibertec.applovepaws.BuildConfig
 import com.cibertec.applovepaws.feature_adopcion.data.api.AdoptionApi
-import com.cibertec.applovepaws.feature_login.data.api.AuthApi
 import com.cibertec.applovepaws.feature_mascota.data.api.MascotaApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -11,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private val baseUrl: String = BuildConfig.API_BASE_URL
+    private const val BASE_URL = "http://10.0.2.2:8080/" // Cambia por tu host
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -21,18 +19,15 @@ object RetrofitClient {
         .addInterceptor(logging)
         .build()
 
+
+
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(baseUrl)
+            .baseUrl(BASE_URL)
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
-
-    val authApi: AuthApi by lazy {
-        retrofit.create(AuthApi::class.java)
-    }
-
     val adoptionApi: AdoptionApi by lazy {
         retrofit.create(AdoptionApi::class.java)
     }
@@ -40,4 +35,5 @@ object RetrofitClient {
     val mascotaApi: MascotaApi by lazy {
         retrofit.create(MascotaApi::class.java)
     }
+
 }
